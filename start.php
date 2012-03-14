01c2d12c7e9c235d267468704c0d2b8e79d0d8d7
Colin Viebrock <colin@viebrock.ca>
2012-03-13 20:59:40 -0500

    A few changes, thanks to k.schmeets.

--- start.php
+++ start.php
@@ -1,29 +1,19 @@
 <?php
 
 
-if (defined('PHPMAILER_LOADED'))
-	return;
-
-
-Laravel\Autoloader::map(array(
-	'PHPMailer' => Bundle::path('phpmailer').'lib/class.phpmailer.php',
+Autoloader::map(array(
+	'PHPMailer' => Bundle::path('phpmailer').'lib'.DS.'class.phpmailer.php',
 ));
 
 
 // Register a mailer in the IoC container
-Laravel\IoC::singleton('mailer', function()
+IoC::singleton('mailer', function()
 {
 
 	$mailer = new PHPMailer();
-
 	$mailer->PluginDir = Bundle::path('phpmailer').'lib';
-
 	$mailer->SetFrom( Config::get('phpmailer::from'), Config::get('phpmailer::from_name'));
 
 	return $mailer;
 
 });
-
-
-
-define('PHPMAILER_LOADED', true);
